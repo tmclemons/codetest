ff05162ee18f292cb8ff59544b8c3c716ae6e177
Tmichael clemons <tmclemons87@gmail.com>
2016-11-17 12:58:14 -0500

    loaded in gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jasmine: {
+            test: {
+                src: [
+                    'bower_components/jasmine/lib/jasmine-core/jasmine.js',
+                    'bower_components/jasmine/lib/jasmine-core/jasmine-html.js',
+                    'bower_components/jasmine/lib/jasmine-core/boot.js',
+                    'src/roverCommands.js'
+                ],
+                options: {
+                    specs: 'spec/rover.spec.js',
+                    display: 'short',
+                    summary: true
+                }
+            }
+        },
+        watch: {
+            files: ['src/roverCommands.js', 'spec/rover.spec.js'],
+            tasks: ['jasmine']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jasmine']);
+
+};
